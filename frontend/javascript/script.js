12980db80c525f4c7523a6191af70cdba874f26f
LeCarteloo <filippapiernik1999@gmail.com>
2021-09-12 17:48:40 +0200

    Added logic for MouseUp event, commented most of the code
    Logic for MouseUp event has been added it now shows the point where user click and where the other point with the same color is located, commented code for better management

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -1,122 +1,177 @@
-var ctx = null;
-var current = null;
-var tileW = 500/5, tileH = 500/5;
-var mapW = 5, mapH = 5;
+var context = null;
+var tileW, tileH, mapW, mapH;
+var canvas = null;
 
 var currentX, currentY, startX, startY;
 selected = Boolean(false);
 win = Boolean(false);
 
-var currentSecond = 0, frameCount = 0, frameLastSecond = 0;
+//Test divs variables
+var current = null;
+var end = null;
+var mouse = null;
 
+// GAME MAP (for test purposes)
 var gameMap = [
     [1, 0, 2, 0, 4],
     [0, 0, 3, 0, 5],
     [0, 0, 0, 0, 0],
-    [0, 2, 0, 4, 0],
+    [0, 0, 2, 4, 0],
     [0, 1, 3, 5, 0],
 ];
+
 window.onload = function() {
-    var canvas = document.getElementById('game');
-    ctx = canvas.getContext("2d");
+    // Getting the canvas and context from the HTML file
+    canvas = document.getElementById('game');
+    context = canvas.getContext("2d");
+
     requestAnimationFrame(drawGame);
-    ctx.font = "bold 10pt sans-serif";
+    context.font = "bold 10pt sans-serif";
+
+    // Adding the event listeners for handling user events
     canvas.addEventListener('mousedown',handleMouseDown);
     canvas.addEventListener('mouseup', handleMouseUp)
 
-    //tests
+    // Height and weight of a tile is equal to size of canvas divided by map size
+    // Map size is always a square
+    tileW = canvas.width/gameMap.length;
+    tileH = canvas.height/gameMap.length;
+    mapW = gameMap.length;
+    mapH = gameMap.length;
+
+    // Debug divs
     current = document.getElementById('status');
+    end = document.getElementById('end');
+    mouse = document.getElementById('mouseMoved');
+
 };
 
+// Fitting the tiles into the size of a canvas
+
+
 function handleMouseUp(event) {
-    console.log("up");
+    // Removing the onmousemove event when mouse button is unpressed
+    canvas.onmousemove = null;
 
-}
+    console.log("Mouse unpressed");
 
-function handleMouseDown(event) {
-    console.log("down");
-    if(!selected) {
-        startX = (Math.floor(event.offsetX / tileW) * tileW) / 100;
-        startY = (Math.floor(event.offsetY / tileH) * tileH) / 100;
-        currentX = startX;
-        currentY = startY;
+    // If tile with point is pressed then 'IF' statement is executed
+    if(selected) {
+        selected = false;
 
-        if(gameMap[startY][startX] > 0) {
-            selected = true;
+        // X and Y coordinate of the second tile with a point with this same color
+        var endX = 0, endY = 0;
+
+        // 'FOR' loops search for a tile with same color
+        for(var y = 0; y < mapH; y++) {
+            for (var x = 0; x < mapW; x++) {
+                // console.log(gameMap[y][x] + ", " + gameMap[startY][startX]);
+                if(!(x == startX && y == startY) && gameMap[y][x] == gameMap[startY][startX]) {
+                    endX = x;
+                    endY = y;
+                }
+            }
         }
     }
 
+    /* Information for debugging
+     Position of clicked rectangle and positon of triangle with the point (same color) */
+    end.innerHTML = "StartX, StartY: " + startX + " " + startY + "<br /> EndX, EndY: " + endX + " " + endY;
+}
 
-    var valueOfY = (Math.floor(event.offsetY / tileW) * tileW) / 100;
-    var valueOfX = (Math.floor(event.offsetX / tileW) * tileW) / 100;
+function handleMouseDown(event) {
+    console.log("Mouse pressed");
 
-    // ctx.beginPath();
-    // ctx.moveTo(valueOfX*tileH + 50, valueOfY*tileW + 50);
-    // ctx.lineTo(50,150);
-    // ctx.lineWidth = 20;
-    // ctx.strokeStyle = "red";
-    // ctx.stroke();
+    // Variables of coordinate of a mouse when mouse is pressed
+    var mouseX = (Math.floor(event.offsetX / tileW) * tileW) / 100;
+    var mouseY = (Math.floor(event.offsetY / tileW) * tileW) / 100;
 
-    // gameMap[valueOfY][valueOfX] = 3;
+    // If no tile is selected then 'IF' statement is executed
+    if(!selected) {
+        startX = mouseX;
+        startY = mouseY;
+        currentX = startX;
+        currentY = startY;
 
-    current.innerHTML = " Rect: " + Math.floor(event.offsetX / tileW) * tileW + 
+        // If tile with a point is selected then selected = true (it prevents from clicking an empty tile)
+        if(gameMap[startY][startX] > 0) {
+            selected = true;
+            // Adding the mousemove event after pressing the button (For test purposes) // it should help to draw a pipe
+            canvas.onmousemove = function(event) {
+                mouse.innerHTML = "Mouse(X, Y): " + event.offsetX + ", " + event.offsetY;
+            }
+        }
+    }
+    else {
+        var x = mouseX;
+        var y = mouseY;
+        if(x >= 0 && y >= 0 && y < gameMap.length && x < gameMap.length && gameMap[y][x]==0) {
+            if( (x == currentX + 1 && y == currentY) || (x == currentX - 1 && y == currentY) 
+            || (x == currentX && y == currentY + 1) || (x == currentX + 1 && y == currentY - 1)) {
+                gameMap[y][x] = gameMap[startY][startX] + 100;
+                currentX = x;
+                currentY = y;           
+            }
+        
+        }
+    }
+
+    // Drawing a line
+    /* context.beginPath();
+    context.moveTo(mouseX*tileH + 50, mouseY*tileW + 50);
+    context.lineTo(50,150);
+    context.lineWidth = 20;
+    context.strokeStyle = "red";
+    context.stroke();
+    gameMap[mouseY][mouseX] = 3; */
+
+    /* Information for debugging
+    Position of clicked rectangle, offset of the mouse (x, y) and full array of the current game map */
+    current.innerHTML = " Rect(X,Y): " + Math.floor(event.offsetX / tileW) * tileW + 
     ", " + Math.floor(event.offsetY / tileW) * tileW + "<br />Offset: " + event.offsetX + ", " + event.offsetY
-    + "<br />Array: " + gameMap + "<br />X, Y: " + valueOfX*tileW + " " + valueOfY*tileH;
+    + "<br />Array: " + gameMap;
 }
 
 function drawGame() {
 
-    if(ctx==null) {
+    if(context==null) {
         return;
-    }
-
-    var sec = Math.floor(Date.now()/1000);
-    
-    if(sec!=currentSecond) {
-        currentSecond = sec;
-        frameLastSecond = frameCount;
-        frameCount = 1;
-    }
-    else {
-        frameCount++;
     }
 
     // Loop drawing a board (with height and width variables)
     for(var y = 0; y < mapH; y++) { 
         for(var x = 0; x < mapW; x++){
-            // Switch statement for colors of the points
+            // Switch statement for colors of the points 
+            // TODO: Move switch statment into the enum with colors later
             switch(gameMap[y][x]) {
-                case 1:
-                    ctx.fillStyle = "red";
+                case 1 || 101:
+                    context.fillStyle = "red";
                     break;
                 case 2:
-                    ctx.fillStyle = "green";
+                    context.fillStyle = "green";
                     break;
                 case 3:
-                    ctx.fillStyle = "blue";
+                    context.fillStyle = "blue";
                     break;
                 case 4:
-                    ctx.fillStyle = "yellow";
+                    context.fillStyle = "yellow";
                     break;
                 case 5:
-                    ctx.fillStyle = "orange";
+                    context.fillStyle = "orange";
                     break;
             }
             
-            ctx.strokeStyle = "#FFF";
-            ctx.strokeRect(x*tileW, y*tileH, tileW, tileH);
+            context.strokeStyle = "#FFF";
+            context.strokeRect(x*tileW, y*tileH, tileW, tileH);
 
             if(gameMap[y][x] != 0) {
-                // ctx.fillRect(x*titleW, y*titleH, titleW, titleH) drawing colors of tiles
+                // context.fillRect(x*titleW, y*titleH, titleW, titleH) <- drawing colors of tiles
                 var circle = new Path2D();
                 circle.moveTo(x*tileW, y*tileH);
-                circle.arc(x * tileW + tileW/2, y * tileH + tileH/2 , 35, 25, 0, 360)
-                ctx.fill(circle);
+                circle.arc(x * tileW + tileW/2, y * tileH + tileH/2 , tileW * .45 , tileH * .45, 0, 360);
+                context.fill(circle);
             }
             
         }
     }
-
-    ctx.fillStyle = "#FFF";
-    ctx.fillText("FPS: " + frameLastSecond, 10, 20);
 }
