16893b56c532df78947681f1290a097aa3e2f963
LeCarteloo <filippapiernik1999@gmail.com>
2022-02-02 18:00:59 +0100

    Lobby selector
    User will be able to join any room without typing the code, atm. lobby selector view is not connected with server

--- client/Lobbies.js
+++ client/Lobbies.js
@@ -1,2 +1,20 @@
-const eee = document.getElementById("test");
-console.log(eee);
+// const socket = io("http://localhost:3000");
+
+function createLobbyElem(hostName, playerCount, status, options) {
+  let div = document.createElement("div");
+  div.classList.add("lobby-card");
+  div.innerHTML = `
+    `;
+  lobbiesList.appendChild(div);
+}
+
+const lobbiesList = document.querySelector(".lobbies-list");
+
+// createLobbyElem();
+// createLobbyElem();
+// createLobbyElem();
+// createLobbyElem();
+// createLobbyElem();
+// createLobbyElem();
+
+const joinBtn = document.getElementById("btn-join");
